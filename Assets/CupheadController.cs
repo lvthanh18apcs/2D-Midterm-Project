2e8caeef9c471a9132ed1defced4b8a233fa6811
lvthanh18apcs <47595741+lvthanh18apcs@users.noreply.github.com>
2021-07-15 21:32:33 +0700

    create quiz template

--- Assets/CupheadController.cs
+++ Assets/CupheadController.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EasyUI.Dialogs;
 
 public class CupheadController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public class Pair
+    {
+        public string door;
+        public string alt_door;
+        public string question;
+        public string title;
+        public string answer;
+        public Pair(string door, string alt_door, string title, string question, string answer)
+        {
+            this.door = door;
+            this.alt_door = alt_door;
+            this.answer = answer;
+            this.title = title;
+            this.question = question;
+        }
+    }
+
     public Rigidbody2D player;
     public Animator anim;
     GameObject m_camera;
     CameraController cam;
+    public GameObject dialogPanel;
+    public GameObject floatingText;
+
 
     string tileset_name = "Dungeon_Tileset_";
     float speed = 4f;
     float x_offset = 0.9906979f, y_offset = 0.4241866f;
     public bool lockDoor = true;
+    public bool Freeze = false;
+    int index = -1;
+    List<Pair> lib;
+    string chestName = "";
+    bool WIN = false;
 
     void Start()
     {
@@ -21,12 +46,19 @@ public class CupheadController : MonoBehaviour
         anim = GetComponent<Animator>();
         m_camera = GameObject.Find("Main Camera");
         cam = (CameraController)m_camera.GetComponent(typeof(CameraController));
+        DialogUI.Instance.Hide();
+        initAnswer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void initAnswer()
     {
-        
+        lib = new List<Pair>();
+        lib.Add(new Pair("E3", "3E", "Key to Room 3", "Test question", ""));
+        lib.Add(new Pair("E1", "1E", "Key to Room 1", "Test question", ""));
+        lib.Add(new Pair("34", "43", "Key to Room 4", "Test question", ""));
+        lib.Add(new Pair("12", "21", "Key to Room 2", "Test question", ""));
+        lib.Add(new Pair("2M", "M2", "Key to Main Chest", "Test question", ""));
+        lib.Add(new Pair("4M", "M4", "Key to Main Room", "Test question", ""));
     }
 
     private void FixedUpdate()
@@ -34,23 +66,26 @@ public class CupheadController : MonoBehaviour
         Quaternion flip = transform.rotation;
         Vector2 velo = new Vector2(0, 0);
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (!Freeze)
         {
-            velo.y = speed;
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            velo.x = -speed;
-            flip.y = 180;
-        }
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            velo.y = -speed;
-        }
-        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            velo.x = speed;
-            flip.y = 0;
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                velo.y = speed;
+            }
+            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                velo.x = -speed;
+                flip.y = 180;
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                velo.y = -speed;
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                velo.x = speed;
+                flip.y = 0;
+            }
         }
         player.velocity = velo;
         transform.rotation = flip;
@@ -71,11 +106,8 @@ public class CupheadController : MonoBehaviour
         transform.position = pos;
     }
 
-    public void openDoor(string name)
+    private void openDoor(string name)
     {
-        string opposite = "";
-        opposite += name[1]; opposite += name[0];
-        openDoor(opposite);
         SpriteRenderer target = GameObject.Find(name).GetComponent<SpriteRenderer>();
         Object[] sprites = Resources.LoadAll("Dungeon_Tileset");
         if (target.sprite.name == tileset_name + "10")
@@ -96,85 +128,178 @@ public class CupheadController : MonoBehaviour
         }
     }
 
+    private void openChest(string name)
+    {
+        SpriteRenderer target = GameObject.Find(name).GetComponent<SpriteRenderer>();
+        Object[] sprites = Resources.LoadAll("Items");
+        if (target.sprite.name == "Items_1")
+        {
+            target.sprite = (Sprite)sprites[1];
+        }
+        else if (target.sprite.name == "Items_2")
+        {
+            target.sprite = (Sprite)sprites[4];
+        }
+    }
+
+    private void showInfo(string info)
+    {
+        if (floatingText)
+        {
+            GameObject floatingPrefab = Instantiate(floatingText, transform.position, Quaternion.identity);
+            floatingPrefab.GetComponentInChildren<TextMesh>().text = info;
+            Destroy(floatingPrefab, 1f);
+        }
+    }
+
+    private bool checkDoorStatus(string sprite_name,string door)
+    {
+        if (sprite_name == tileset_name + "12" || sprite_name == tileset_name + "10" || sprite_name == tileset_name + "11" || sprite_name == tileset_name + "13")
+        {
+            showInfo("Cannot go through here yet");
+            return false;
+        }
+        return true;
+    }
+
+    public void update_input(string input)
+    {
+        DialogUI.Instance.Hide();
+        Freeze = false;
+        if (input == lib[index].answer)
+        {
+            openDoor(lib[index].door);
+            openDoor(lib[index].alt_door);
+            openChest(chestName);
+            string keyword = lib[index].door;
+            switch (keyword)
+            {
+                case "E3":
+                    showInfo("Room 3 is unlocked");
+                    break;
+                case "E1":
+                    showInfo("Room 1 is unlocked");
+                    break;
+                case "34":
+                    showInfo("Room 4 is unlocked");
+                    break;
+                case "12":
+                    showInfo("Room 2 is unlocked");
+                    break;
+                case "4M":
+                    showInfo("Main Room is unlocked");
+                    break;
+                case "2M":
+                    showInfo("Main Chest is unlocked");
+                    WIN = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void hitChest(string chest)
+    {
+        if (WIN && chest == "WINCHEST")
+        {
+            anim.SetBool("win", true);
+            openChest(chest);
+        }
+
+        if (chest.Length < 5)
+            return;
+        index = -1;
+        chestName = "";
+        string chestStatus = GameObject.Find(chest).GetComponent<SpriteRenderer>().sprite.name;
+        if (chestStatus == "Items_3" || chestStatus == "Items_0")
+        {
+            showInfo("I have already looted this chest");
+            return;
+        }
+        string door = "";
+        door += chest[3]; door += chest[4];
+        for(int i =0;i<lib.Count;++i)
+            if (lib[i].door == door || lib[i].alt_door == door)
+            {
+                index = i;
+                chestName = chest;
+                break;
+            }
+        if (index == -1)
+        {
+            showInfo("There must be something in here");
+            return;
+        }
+        DialogUI.Instance.setTitle(lib[index].title).setQuestion(lib[index].question).Show();
+        Freeze = true;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         SpriteRenderer valid;
-        string door = collision.gameObject.name;
-        //Debug.Log(collision.gameObject.name);
-        valid = GameObject.Find(door).GetComponent<SpriteRenderer>();
-        switch (door)
+        string c_object = collision.gameObject.name;
+        valid = GameObject.Find(c_object).GetComponent<SpriteRenderer>();
+        switch (c_object)
         {
             case "EM":
-                if (valid.sprite.name == tileset_name + "12" && lockDoor)
-                    break;
-                Teleport("MainRoom",true,-5);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("MainRoom",true,-5);
                 break;
             case "ME":
-                if (valid.sprite.name == tileset_name + "11" && lockDoor)
-                    break;
-                Teleport("EntranceRoom", true, 5);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("EntranceRoom", true, 5);
                 break;
             case "E1":
-                if (valid.sprite.name == tileset_name + "10" && lockDoor)
-                    break;
-                Teleport("Room1", false, -2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room1", false, -2);
                 break;
             case "1E":
-                if (valid.sprite.name == tileset_name + "13" && lockDoor)
-                    break;
-                Teleport("EntranceRoom", false, 2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("EntranceRoom", false, 2);
                 break;
             case "E3":
-                if (valid.sprite.name == tileset_name + "13" && lockDoor)
-                    break;
-                Teleport("Room3", false, 2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room3", false, 2);
                 break;
             case "3E":
-                if (valid.sprite.name == tileset_name + "10" && lockDoor)
-                    break;
-                Teleport("EntranceRoom", false, -2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("EntranceRoom", false, -2);
                 break;
             case "12":
-                if (valid.sprite.name == tileset_name + "12" && lockDoor)
-                    break;
-                Teleport("Room2", true, -5);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room2", true, -5);
                 break;
             case "21":
-                if (valid.sprite.name == tileset_name + "11" && lockDoor)
-                    break;
-                Teleport("Room1", true, 5);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room1", true, 5);
                 break;
             case "2M":
-                if (valid.sprite.name == tileset_name + "13" && lockDoor)
-                    break;
-                Teleport("MainRoom", false, 2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("MainRoom", false, 2);
                 break;
             case "M2":
-                if (valid.sprite.name == tileset_name + "10" && lockDoor)
-                    break;
-                Teleport("Room2", false, -2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room2", false, -2);
                 break;
             case "34":
-                if (valid.sprite.name == tileset_name + "12" && lockDoor)
-                    break;
-                Teleport("Room4", true, -5);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room4", true, -5);
                 break;
             case "43":
-                if (valid.sprite.name == tileset_name + "11" && lockDoor)
-                    break;
-                Teleport("Room3", true, 5);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room3", true, 5);
                 break;
             case "4M":
-                if (valid.sprite.name == tileset_name + "10" && lockDoor)
-                    break;
-                Teleport("MainRoom", false, -2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("MainRoom", false, -2);
                 break;
             case "M4":
-                if (valid.sprite.name == tileset_name + "13" && lockDoor)
-                    break;
-                Teleport("Room4", false, 2);
+                if (checkDoorStatus(valid.sprite.name, c_object))
+                    Teleport("Room4", false, 2);
                 break;
             default:
+                hitChest(c_object);
                 break;
         }
     }
---
+++ Assets/DialogUI.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EasyUI.Dialogs
+{
+    public class Dialog
+    {
+        public string title;
+        public string question;
+    }
+    public class DialogUI : MonoBehaviour
+    {
+        [SerializeField] UnityEngine.UI.Text title;
+        [SerializeField] UnityEngine.UI.Text question;
+        [SerializeField] UnityEngine.UI.Button button;
+        [SerializeField] GameObject canvas;
+
+        GameObject player;
+        GameObject cam;
+
+        Dialog dialog = new Dialog();
+
+        public static DialogUI Instance;
+
+        void Awake()
+        {
+            Instance = this;
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(Hide);
+            player = GameObject.Find("Cuphead");
+            cam = GameObject.Find("Main Camera");
+        }
+
+        public DialogUI setTitle(string title)
+        {
+            dialog.title = title;
+            return Instance;
+        }
+
+        public DialogUI setQuestion(string question)
+        {
+            dialog.question = question;
+            return Instance;
+        }
+
+        public void Show()
+        {
+            title.text = dialog.title;
+            question.text = dialog.question;
+            dialog = new Dialog();
+            canvas.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            player.GetComponent<CupheadController>().Freeze = false;
+            canvas.SetActive(false);
+            dialog = new Dialog();
+        }
+
+        private void FixedUpdate()
+        {
+            if (transform.position != cam.transform.position)
+                transform.position = cam.transform.position;
+        }
+    }
+}
